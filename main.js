284832d3e1788cb40c7dd5e274d77753973edb0e
Eduard <dereddyteddy@gmail.com>
2015-03-05 11:36:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ enchant();
 window.onload = function() {
     var game = new Game(320, 320);
     game.fps = 20;
-    game.preload('chara1.png', 'map0.png');
+    game.preload('https://raw.githubusercontent.com/wise9/enchant.js/master/examples/beginner/map/chara1.png', 'https://raw.githubusercontent.com/wise9/enchant.js/master/examples/beginner/map/map0.png');
     game.onload = function() {
         var map = new Map(16, 16);
-        map.image = game.assets['map0.png'];
+        map.image = game.assets['https://raw.githubusercontent.com/wise9/enchant.js/master/examples/beginner/map/map0.png'];
         map.loadData(
             [
                 [4, 4, 4, 4, 4, 4, 4],
